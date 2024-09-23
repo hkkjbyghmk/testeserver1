ba51e3e33290709f5dbf1b9602311e862abdd01e
hkkjbyghmk <69396490+hkkjbyghmk@users.noreply.github.com>
2024-09-23 14:37:38 +0100

    Update app.py
    Signed-off-by: hkkjbyghmk <69396490+hkkjbyghmk@users.noreply.github.com>

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ VENDA_VALUE = 27  # Valor para "Venda"
 COMPRA_VALUE = -12  # Valor para "Compra"
 ADMIN_CODE = '7851'  # Código para liberar o administrador
 
+# Rota para obter os valores de venda e compra
 @app.route('/config')
 def get_config():
     try:
@@ -16,26 +17,29 @@ def get_config():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+# Rota para verificar o código do administrador
 @app.route('/check_code', methods=['POST'])
 def check_code():
-    # Verifique se a requisição é do tipo POST
-    if request.method == 'POST':
-        # Tente capturar o JSON da requisição
-        data = request.get_json()
+    try:
+        # Verifique se a requisição é do tipo POST
+        if request.method == 'POST':
+            # Tente capturar o JSON da requisição
+            data = request.get_json()
 
-        # Verifica se o campo 'code' foi enviado na requisição
-        if not data or 'code' not in data:
-            return jsonify({"status": "failure", "message": "Código não fornecido"}), 400
+            # Verifica se o campo 'code' foi enviado na requisição
+            if not data or 'code' not in data:
+                return jsonify({"status": "failure", "message": "Código não fornecido"}), 400
 
-        # Obtém o código enviado e verifica
-        code = data.get('code')
-        if code == ADMIN_CODE:
-            return jsonify({"status": "success", "message": "Código correto"})
-        else:
-            return jsonify({"status": "failure", "message": "Código incorreto"})
-    else:
-        return jsonify({"status": "failure", "message": "Método não permitido"}), 405
+            # Obtém o código enviado e verifica
+            code = data.get('code')
+            if code == ADMIN_CODE:
+                return jsonify({"status": "success", "message": "Código correto"})
+            else:
+                return jsonify({"status": "failure", "message": "Código incorreto"})
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
 
+# Rota para servir o arquivo HTML (index.html)
 @app.route('/')
 def serve_index():
     return send_from_directory(os.getcwd(), 'index.html')
